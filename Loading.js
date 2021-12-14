ca41aeb4660a87bbae6004b7cf18bf08e0452961
JoySystem_v <fvv2011@gmail.com>
2021-12-14 23:57:14 +0300

    Make first component

---
+++ Loading.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function Loading() {
+
+    return (
+    <View style={styles.container}>
+<Text style={styles.text}>
+    Получение погоды...
+</Text>
+
+    </View>);
+
+
+}
+
+
+const styles = StyleSheet.create({
+
+container:{
+    flex: 1,
+    justifyContent: 'flex-end',
+    //paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: '#fdf6aa',
+    alignItems: 'center',
+
+},
+text:{
+    color:"#2c2c2c",
+    fontSize: 30,
+}
+
+
+})
